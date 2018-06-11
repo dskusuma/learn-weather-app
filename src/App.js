e4d57854e100da3f571d66cdf10f62a92bf17552
dskusuma <dwisatriakusuma4@gmail.com>
2018-06-11 16:32:26 +0700

    Add Weather API_KEY and getWeather method

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+// OpenWeatherMap API KEY : 
+const API_KEY = 'ee185aee02f3686c7535e4447961175b';
+
+// URL BANDUNG : http://api.openweathermap.org/data/2.5/weather?id=1650357&appid=ee185aee02f3686c7535e4447961175b&units=metric
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=1650357&appid=${API_KEY}&units=metric`);
+
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
         <Titles />
         <Form />
-        <p>Hello!</p>
-        <h1>YO!</h1>
+        <Weather />
       </div>
     );
   }
