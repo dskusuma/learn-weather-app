03c6c27b48742306315b4322da930c0d4a70d0da
dskusuma <dwisatriakusuma4@gmail.com>
2018-06-11 17:06:04 +0700

    update Weather.js to show the properties

--- src/App.js
+++ src/App.js
@@ -10,25 +10,46 @@ const API_KEY = 'ee185aee02f3686c7535e4447961175b';
 // URL BANDUNG : http://api.openweathermap.org/data/2.5/weather?id=1650357&appid=ee185aee02f3686c7535e4447961175b&units=metric
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     // prevent Default behaviour
     e.preventDefault();
-
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-
     const data = await api_call.json();
     console.log(data);
-  }
 
+    // Don't manipulate directly to the object
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+  }
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
