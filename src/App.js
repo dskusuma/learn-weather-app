41c1ac87c613ad6e597c7fbb3be0d7107775a8b1
dskusuma <dwisatriakusuma4@gmail.com>
2018-06-11 16:46:06 +0700

    update component Form and dynamic API call in App

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ const API_KEY = 'ee185aee02f3686c7535e4447961175b';
 // URL BANDUNG : http://api.openweathermap.org/data/2.5/weather?id=1650357&appid=ee185aee02f3686c7535e4447961175b&units=metric
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=1650357&appid=${API_KEY}&units=metric`);
+  getWeather = async (e) => {
+    // prevent Default behaviour
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
 
     const data = await api_call.json();
     console.log(data);
@@ -21,7 +27,7 @@ class App extends React.Component {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
