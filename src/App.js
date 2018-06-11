6047f3e1e9cb207c7155a4d169ff6ec5646312d4
dskusuma <dwisatriakusuma4@gmail.com>
2018-06-11 15:47:32 +0700

    Initialize Form.js and import Titles and Form to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Titles from './components/Titles';
+import Form from './components/Form';
+
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Titles />
+        <Form />
         <p>Hello!</p>
         <h1>YO!</h1>
       </div>
